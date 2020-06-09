8e6d8efe658598a0fb0c0077c1fc8a1accc97f59
Seamus Brady <seamus@corvideon.ie>
2020-06-10 00:21:16 +0100

    Initial algorithms

--- src/app/py_causal_wrapper.py
+++ src/app/py_causal_wrapper.py
@@ -1,10 +1,14 @@
 """
 TBD Header
 """
+import logging
 
+from pycausal import prior as p
+from pycausal import search as s
 from pycausal.pycausal import pycausal
-from app.causal_discovery_algo_enum import CausalDiscoveryAlgos
-from pycausal import search
+
+_logger = logging.getLogger(__name__)
+
 
 class PyCausalWrapper():
     """
@@ -25,7 +29,6 @@ class PyCausalWrapper():
         """
         self.pc.start_vm()
 
-
     def stop_vm(self):
         """
         TBD
@@ -38,7 +41,7 @@ class PyCausalWrapper():
         TBD
         :return:
         """
-        return search.tetradrunner()
+        return s.tetradrunner()
 
     def get_causal_discovery_algos(self):
         """
@@ -50,3 +53,107 @@ class PyCausalWrapper():
         for k, v in algo_dict.items():
             causal_discovery_algos.append(k)
         return causal_discovery_algos
+
+    def dump_all_algo_desc(self):
+        """
+        TBD
+        :return:
+        """
+        for algo in self.get_causal_discovery_algos():
+            print("------------------------------------------------------------------------")
+            print("Algorithm ID: ", algo)
+            print("Description: ", algo)
+            self.get_tetrad().getAlgorithmDescription(algo)
+
+    # -------------------------------------------------------------------------------------------------
+    #                   The methods below return causal discovery algorithms....
+    # -------------------------------------------------------------------------------------------------
+
+    def algo_bayes_est(self, df):
+        """
+        bayesEst is the revised Greedy Equivalence Search (GES) algorithm developed
+        by Joseph D. Ramsey, Director of Research Computing, Department of Philosophy,
+        Carnegie Mellon University, Pittsburgh, PA.
+        :param df: dataframe
+        :return: dot graph string
+        """
+        dot_str = None
+        try:
+            bayes_est = s.bayesEst(df, depth=-1, alpha=0.05, verbose=True)
+            if bayes_est.getTetradGraph() is not None:
+                dot_str = self.pc.tetradGraphToDot(bayes_est.getTetradGraph())
+        except Exception as e:
+            _logger.error(str(e))
+            print(str(e))
+        return dot_str
+
+    def algo_fges_continuous(self, df):
+        """
+        FGES is an optimized and parallelized version of an algorithm developed by Meek [Meek, 1997]
+        called the Greedy Equivalence Search (GES). The algorithm was further developed and studied
+        by Chickering [Chickering, 2002]. GES is a Bayesian algorithm that heuristically searches
+        the space of CBNs and returns the model with highest Bayesian score it finds. In particular,
+        GES starts its search with the empty graph. It then performs a forward stepping search in
+        which edges are added between nodes in order to increase the Bayesian score. This process
+        continues until no single edge addition increases the score. Finally, it performs a backward
+        stepping search that removes edges until no single edge removal can increase the score.
+        The algorithms requires a decomposable score—that is, a score that for the entire DAG model
+        is a sum of logged scores of each variables given its parents in the model. The algorithms
+        can take all continuous data (using the SEM BIC score), all discrete data
+        (using the BDeu score) or a mixture of continuous and discrete data
+        (using the Conditional Gaussian score); these are all decomposable scores.
+        :param df: dataframe
+        :return: dot graph string
+        """
+        dot_str = None
+        # pass in empty prior
+        forbid = []
+        require = []
+        tempForbid = p.ForbiddenWithin([])
+        temporal = [tempForbid]
+        try:
+            prior = p.knowledge(forbiddirect=forbid, requiredirect=require, addtemporal=temporal)
+            self.get_tetrad().run(algoId='fges', dfs=df, scoreId='sem-bic', dataType='continuous',
+                                  maxDegree=-1, faithfulnessAssumed=True, verbose=True)
+            if self.get_tetrad().getTetradGraph() is not None:
+                dot_str = self.pc.tetradGraphToDot(self.get_tetrad().getTetradGraph())
+        except Exception as e:
+            _logger.error(str(e))
+            print(str(e))
+        return dot_str
+
+    def algo_fges_discrete(self, df):
+        """
+        FGES is an optimized and parallelized version of an algorithm developed by Meek [Meek, 1997]
+        called the Greedy Equivalence Search (GES). The algorithm was further developed and studied
+        by Chickering [Chickering, 2002]. GES is a Bayesian algorithm that heuristically searches
+        the space of CBNs and returns the model with highest Bayesian score it finds. In particular,
+        GES starts its search with the empty graph. It then performs a forward stepping search in
+        which edges are added between nodes in order to increase the Bayesian score. This process
+        continues until no single edge addition increases the score. Finally, it performs a backward
+        stepping search that removes edges until no single edge removal can increase the score.
+        The algorithms requires a decomposable score—that is, a score that for the entire DAG model
+        is a sum of logged scores of each variables given its parents in the model. The algorithms
+        can take all continuous data (using the SEM BIC score), all discrete data
+        (using the BDeu score) or a mixture of continuous and discrete data
+        (using the Conditional Gaussian score); these are all decomposable scores.
+        :param df: dataframe
+        :return: dot graph string
+        """
+        dot_str = None
+        # pass in empty prior
+        forbid = []
+        require = []
+        tempForbid = p.ForbiddenWithin([])
+        temporal = [tempForbid]
+        try:
+            prior = p.knowledge(forbiddirect=forbid, requiredirect=require, addtemporal=temporal)
+            self.get_tetrad().run(algoId='fges', dfs=df, scoreId='bdeu-score', priorKnowledge=prior,
+                                  dataType='discrete', maxDegree=3, faithfulnessAssumed=True, verbose=True,
+                                  numberResampling=5, resamplingEnsemble=1, addOriginalDataset=True)
+            if self.get_tetrad().getTetradGraph() is not None:
+                dot_str = self.pc.tetradGraphToDot(self.get_tetrad().getTetradGraph())
+        except Exception as e:
+            _logger.error(str(e))
+            print(str(e))
+        return dot_str
--- src/test/test_py_causal_wrapper.py
+++ src/test/test_py_causal_wrapper.py
@@ -1,5 +1,7 @@
+import os
+
 from tests.unit import TestAPI
-import time
+import pandas as pd
 from app.py_causal_wrapper import PyCausalWrapper
 
 
@@ -10,7 +12,7 @@ class Test_PyCausalWrapper(TestAPI):
     wrapper = PyCausalWrapper()
 
     def setUp(self):
-        pass
+        self.data_dir = os.path.join(os.path.dirname(__file__), 'resources/data')
 
     def tearDown(self):
         pass
@@ -29,3 +31,34 @@ class Test_PyCausalWrapper(TestAPI):
             causal_discovery_algos.append(algo)
         self.wrapper.stop_vm()
         self.assertTrue(len(causal_discovery_algos) == 25)
+
+
+    def test_get_algos(self):
+        self.wrapper.start_vm()
+        self.wrapper.dump_all_algo_desc()
+        self.wrapper.stop_vm()
+        self.assertTrue(True)
+
+    def test_algo_bayes_est(self):
+        data_dir = os.path.join(self.data_dir, "sim_discrete_data_20vars_100cases.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        self.wrapper.start_vm()
+        dot_str = self.wrapper.algo_bayes_est(df)
+        self.wrapper.stop_vm()
+        self.assertTrue(dot_str is not None, "No graph returned.")
+
+    def test_algo_fges_continuous(self):
+        data_dir = os.path.join(self.data_dir, "charity.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        self.wrapper.start_vm()
+        dot_str = self.wrapper.algo_fges_continuous(df)
+        self.wrapper.stop_vm()
+        self.assertTrue(dot_str is not None, "No graph returned.")
+
+    def test_algo_fges_discrete(self):
+        data_dir = os.path.join(self.data_dir, "audiology.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        self.wrapper.start_vm()
+        dot_str = self.wrapper.algo_fges_discrete(df)
+        self.wrapper.stop_vm()
+        self.assertTrue(dot_str is not None, "No graph returned.")
