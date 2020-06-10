0ebb2bee9a9c6f3afe833f3368602ff446eac1cf
Seamus Brady <seamus@corvideon.ie>
2020-06-10 08:35:38 +0100

    multiple tests running sequentially!

--- src/app/py_causal_wrapper.py
+++ src/app/py_causal_wrapper.py
@@ -16,60 +16,32 @@ class PyCausalWrapper():
     """
 
     def __init__(self):
-        """
-        TBD
-        :return:
-        """
-        self.pc = pycausal()
-
-    def start_vm(self):
-        """
-        TBD
-        :return:
-        """
-        self.pc.start_vm()
-
-    def stop_vm(self):
-        """
-        TBD
-        :return:
-        """
-        self.pc.stop_vm()
-
-    def get_tetrad(self):
-        """
-        TBD
-        :return:
-        """
-        return s.tetradrunner()
+        pass
 
     def get_causal_discovery_algos(self):
         """
         TBD
         :return:
         """
+        # start java vm and get algo runner
+        pc = pycausal()
+        pc.start_vm()
+        tetrad = s.tetradrunner()
+
         causal_discovery_algos = []
-        algo_dict = self.get_tetrad().algos
+        algo_dict = tetrad.algos
         for k, v in algo_dict.items():
             causal_discovery_algos.append(k)
-        return causal_discovery_algos
 
-    def dump_all_algo_desc(self):
-        """
-        TBD
-        :return:
-        """
-        for algo in self.get_causal_discovery_algos():
-            print("------------------------------------------------------------------------")
-            print("Algorithm ID: ", algo)
-            print("Description: ", algo)
-            self.get_tetrad().getAlgorithmDescription(algo)
+        # stop java vm
+        pc.stop_vm()
+        return causal_discovery_algos
 
     # -------------------------------------------------------------------------------------------------
     #                   The methods below return causal discovery algorithms....
     # -------------------------------------------------------------------------------------------------
 
-    def algo_bayes_est(self, df):
+    def algo_bayes_est(self, df, pc = None):
         """
         bayesEst is the revised Greedy Equivalence Search (GES) algorithm developed
         by Joseph D. Ramsey, Director of Research Computing, Department of Philosophy,
@@ -79,15 +51,24 @@ class PyCausalWrapper():
         """
         dot_str = None
         try:
+            # start java vm and get algo runner
+            if pc is None:
+                pc = pycausal()
+                pc.start_vm()
+
             bayes_est = s.bayesEst(df, depth=-1, alpha=0.05, verbose=True)
-            if bayes_est.getTetradGraph() is not None:
-                dot_str = self.pc.tetradGraphToDot(bayes_est.getTetradGraph())
+            graph = bayes_est.getTetradGraph()
+            dot_str = pc.tetradGraphToDot(graph)
+
+            # stop java vm
+            if pc is None:
+                pc.stop_vm()
         except Exception as e:
             _logger.error(str(e))
             print(str(e))
         return dot_str
 
-    def algo_fges_continuous(self, df):
+    def algo_fges_continuous(self, df, pc = None):
         """
         FGES is an optimized and parallelized version of an algorithm developed by Meek [Meek, 1997]
         called the Greedy Equivalence Search (GES). The algorithm was further developed and studied
@@ -105,24 +86,30 @@ class PyCausalWrapper():
         :param df: dataframe
         :return: dot graph string
         """
+        single_run = False
         dot_str = None
-        # pass in empty prior
-        forbid = []
-        require = []
-        tempForbid = p.ForbiddenWithin([])
-        temporal = [tempForbid]
         try:
-            prior = p.knowledge(forbiddirect=forbid, requiredirect=require, addtemporal=temporal)
-            self.get_tetrad().run(algoId='fges', dfs=df, scoreId='sem-bic', dataType='continuous',
-                                  maxDegree=-1, faithfulnessAssumed=True, verbose=True)
-            if self.get_tetrad().getTetradGraph() is not None:
-                dot_str = self.pc.tetradGraphToDot(self.get_tetrad().getTetradGraph())
+            # start java vm and get algo runner
+            if pc is None:
+                pc = pycausal()
+                pc.start_vm()
+                single_run = True
+
+            tetrad = s.tetradrunner()
+            tetrad.run(algoId='fges', dfs=df, scoreId='sem-bic', dataType='continuous',
+                       maxDegree=-1, faithfulnessAssumed=True, verbose=True)
+            graph = tetrad.getTetradGraph()
+            dot_str = pc.tetradGraphToDot(graph)
+
+            # shutdown java vm
+            if single_run:
+                pc.stop_vm()
         except Exception as e:
             _logger.error(str(e))
             print(str(e))
         return dot_str
 
-    def algo_fges_discrete(self, df):
+    def algo_fges_discrete(self, df, pc=None):
         """
         FGES is an optimized and parallelized version of an algorithm developed by Meek [Meek, 1997]
         called the Greedy Equivalence Search (GES). The algorithm was further developed and studied
@@ -140,19 +127,24 @@ class PyCausalWrapper():
         :param df: dataframe
         :return: dot graph string
         """
+        single_run = False
         dot_str = None
-        # pass in empty prior
-        forbid = []
-        require = []
-        tempForbid = p.ForbiddenWithin([])
-        temporal = [tempForbid]
         try:
-            prior = p.knowledge(forbiddirect=forbid, requiredirect=require, addtemporal=temporal)
-            self.get_tetrad().run(algoId='fges', dfs=df, scoreId='bdeu-score', priorKnowledge=prior,
-                                  dataType='discrete', maxDegree=3, faithfulnessAssumed=True, verbose=True,
-                                  numberResampling=5, resamplingEnsemble=1, addOriginalDataset=True)
-            if self.get_tetrad().getTetradGraph() is not None:
-                dot_str = self.pc.tetradGraphToDot(self.get_tetrad().getTetradGraph())
+            # start java vm and get algo runner
+            if pc is None:
+                pc = pycausal()
+                pc.start_vm()
+                single_run = True
+
+            tetrad = s.tetradrunner()
+            tetrad.run(algoId='fges', dfs=df, scoreId='cg-bic-score', dataType='discrete',
+                       maxDegree=3, faithfulnessAssumed=True, symmetricFirstStep=True, verbose=True)
+            graph = tetrad.getTetradGraph()
+            dot_str = pc.tetradGraphToDot(graph)
+
+            # shutdown java vm
+            if single_run:
+                pc.stop_vm()
         except Exception as e:
             _logger.error(str(e))
             print(str(e))
--- src/test/test_py_causal_wrapper.py
+++ src/test/test_py_causal_wrapper.py
@@ -3,6 +3,8 @@ import os
 from tests.unit import TestAPI
 import pandas as pd
 from app.py_causal_wrapper import PyCausalWrapper
+from pycausal import search as s
+from pycausal.pycausal import pycausal
 
 
 class Test_PyCausalWrapper(TestAPI):
@@ -25,40 +27,50 @@ class Test_PyCausalWrapper(TestAPI):
 
 
     def test_get_algos(self):
-        self.wrapper.start_vm()
         causal_discovery_algos = []
         for algo in self.wrapper.get_causal_discovery_algos():
             causal_discovery_algos.append(algo)
-        self.wrapper.stop_vm()
         self.assertTrue(len(causal_discovery_algos) == 25)
 
-
-    def test_get_algos(self):
-        self.wrapper.start_vm()
-        self.wrapper.dump_all_algo_desc()
-        self.wrapper.stop_vm()
-        self.assertTrue(True)
 
     def test_algo_bayes_est(self):
         data_dir = os.path.join(self.data_dir, "sim_discrete_data_20vars_100cases.txt")
         df = pd.read_table(data_dir, sep="\t")
-        self.wrapper.start_vm()
         dot_str = self.wrapper.algo_bayes_est(df)
-        self.wrapper.stop_vm()
         self.assertTrue(dot_str is not None, "No graph returned.")
 
     def test_algo_fges_continuous(self):
         data_dir = os.path.join(self.data_dir, "charity.txt")
         df = pd.read_table(data_dir, sep="\t")
-        self.wrapper.start_vm()
         dot_str = self.wrapper.algo_fges_continuous(df)
-        self.wrapper.stop_vm()
         self.assertTrue(dot_str is not None, "No graph returned.")
 
     def test_algo_fges_discrete(self):
         data_dir = os.path.join(self.data_dir, "audiology.txt")
         df = pd.read_table(data_dir, sep="\t")
-        self.wrapper.start_vm()
         dot_str = self.wrapper.algo_fges_discrete(df)
-        self.wrapper.stop_vm()
         self.assertTrue(dot_str is not None, "No graph returned.")
+
+    def test_multiple_algo_run(self):
+        dot_str_list = []
+
+        pc = pycausal()
+        pc.start_vm()
+        tetrad = s.tetradrunner()
+
+        data_dir = os.path.join(self.data_dir, "sim_discrete_data_20vars_100cases.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        dot_str_list.append(self.wrapper.algo_bayes_est(df, pc))
+
+        data_dir = os.path.join(self.data_dir, "charity.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        dot_str_list.append(self.wrapper.algo_fges_continuous(df, pc))
+
+        data_dir = os.path.join(self.data_dir, "audiology.txt")
+        df = pd.read_table(data_dir, sep="\t")
+        dot_str_list.append(self.wrapper.algo_fges_discrete(df, pc))
+
+        pc.stop_vm()
+
+        self.assertTrue(len(dot_str_list) == 3)
+
